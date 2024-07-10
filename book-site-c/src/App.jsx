acda11a6c661f110459ebec4f597cf1f71221030
MiuX1 <samsaikia97@gmail.com>
2024-07-10 21:41:03 +0530

    first commit (basic setup with react+vite, tailwin and mui insatlled)

---
+++ book-site-c/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import ResponsiveAppBar from '../pages/home'
+
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom'
+import Login from '../pages/login'
+import Test from '../pages/test'
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ResponsiveAppBar/>}/>
+          <Route path='/:login/1' element={<Login/>}/>
+          <Route path='/:test/2' element={<Test/>}/>
+        </Routes>
+      </BrowserRouter> 
+   </>
+  )
+}
+
+export default App
