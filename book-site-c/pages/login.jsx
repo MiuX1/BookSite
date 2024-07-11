d7b8b67b6d57b7c0f0024013b019c8a7f7d38bce
MiuX1 <samsaikia97@gmail.com>
2024-07-11 23:10:22 +0530

    footer component built and added to login.jsx

--- book-site-c/pages/login.jsx
+++ book-site-c/pages/login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../src/components/Header/Header'
+import Footer from '../src/components/Footer/Footer'
 const Login = () => {
   return (
     <>
      <Header/>
+     <Footer/>
     </>
   )
 }
